fa708f5f7151104260c8c4d3e8fcf02b9f743039
igor <ftain44@mail.ru>
2023-06-16 13:46:02 +0300

    дела делишки

---
+++ Task34.py
@@ -0,0 +1,9 @@
+'Задача 34: Винни-Пух попросил Вас посмотреть, есть ли в его стихах ритм.'
+alp = "аеёиоуыэюя"
+word_sug = input().split()
+vowel_letters = [sum([True for j in word if j.lower() in alp]) for word in word_sug]
+
+if all(vowel_letters) and len(set(vowel_letters)) == 1:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
